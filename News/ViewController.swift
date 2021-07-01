223a93caa2b107dec3ea6fb55c7bf45c09faad7c
Rafael <rhb.ortiz@gmail.com>
2021-07-01 19:57:20 -0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Rafael Ortiz on 01/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
